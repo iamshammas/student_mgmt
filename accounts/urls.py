b85b748cd18906a870436f4660f128f141634d8a
SHAMMAS <muhdshammas612@gmail.com>
2025-11-19 20:04:43 +0530

    changing template

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('',views.index,name='index'),
     path('login/',views.user_login,name='user_login'),
+    path('home/',views.home,name='home'),
+    path('user_register/',views.user_register,name='user_register'),
+    path('profile/',views.profile,name='profile'),
+    path('logout/',views.logout,name='logout'),
+    path('password_reset/',views.password_reset,name='password_reset'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -7,3 +8,18 @@ def index(request):
 
 def user_login(request):
     return render(request,'user_login.html')
+
+def home(request):
+    return HttpResponse('HELLO WORK')
+
+def user_register(request):
+    return HttpResponse('HELLO WORK')
+
+def profile(request):
+    return HttpResponse('HELLO WORK')
+
+def logout(request):
+    return HttpResponse('HELLO WORK')
+
+def password_reset(request):
+    return HttpResponse('HELLO WORK')
